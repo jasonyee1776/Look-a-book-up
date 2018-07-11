2d4e52c63d33a50b3fe59727af7aeb74bfdd7b97
Jason Yee <jasonyee1776@gmail.com>
2018-07-10 19:44:46 -0700

    updated funtionality

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 document.getElementById('search').addEventListener("keyup", function (event) {
     event.preventDefault();
     if (event.keyCode === 13) {
-        document.getElementById('searchButton').click();
+        document.getElementById('search-button').click();
     }
 });
 
